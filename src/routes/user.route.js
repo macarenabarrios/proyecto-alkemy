d84975bf4b49af31c517393a13e45e976ce7ab91
Federico Silva <fedes7777@gmail.com>
2023-12-01 16:42:30 -0300

    merge conflicts

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,14 +4,9 @@ import { hasAnyRole, isAccountOwner } from '../middleware/index.js';
 
 const router = Router();
 
-<<<<<<< HEAD
 
 router.get('',hasAnyRole(["ADMIN"]), getAll);
 router.get('/:id',isAccountOwner, hasAnyRole(["ADMIN", "USER"]), getById);
-=======
-router.get('', getAll);
-router.get('/:id', hasAnyRole(["ADMIN", "USER"]), getById);
->>>>>>> cf4becd0f7a3886498218024120ce77d4f242205
 router.post('', save);
 router.put('/:id',isAccountOwner, update);
 router.delete('/:id',isAccountOwner, deleteUser);
