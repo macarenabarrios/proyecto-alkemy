260f7b15de99ffe18a4ecaa3dce6326abcfdc2c5
Federico Silva <fedes7777@gmail.com>
2023-12-06 13:56:58 -0300

    chg user update

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import indexRouter from './src/routes/index.route.js'
 import seed from './src/db/seed.db.js';
-import ApplicationError from './src/exceptions/ApplicationError.js';
 import errorHandler from './src/middleware/error.middleware.js';
 
 
@@ -38,9 +37,13 @@ main();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`app listening on port ${PORT}!`);
+});
+app.use((req, res, next) => {
+  const err = new Error('Path not Found');
+  err.status = 404;
+  next(err);
 });
 app.use(express.json());
 app.use(cookieParser());
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { deleteUser, getAll, getById, save, update } from '../controllers/user.controller.js';
-import { hasAnyRole, isAccountOwner } from '../middleware/index.js';
+import { hasAnyRole, isAccountOwner, extractAuthenticated } from '../middleware/index.js';
 
 const router = Router();
 
@@ -8,7 +8,7 @@ const router = Router();
 router.get('', hasAnyRole(["ADMIN"]), getAll);
 router.get('/:id', isAccountOwner, hasAnyRole(["ADMIN", "USER"]), getById);
 router.post('', save);
-router.put('/:id', isAccountOwner, update);
+router.put('', isAccountOwner,extractAuthenticated, update);
 router.delete('/:id', isAccountOwner, deleteUser);
 
 export default router;
